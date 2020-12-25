f9b2727f45e29d2dca046c5c7354b0ca56677519
Manjunath <Manjunath@version1.com>
2020-12-25 00:00:09 +0000

    Singletone pattern example

---
+++ src/main/java/com/manjunath/dotikol/designpattern/singletone/ABC.java
@@ -0,0 +1,24 @@
+package com.manjunath.dotikol.designpattern.singletone;
+
+public class ABC {
+
+    private ABC() {
+        System.out.println("Instance created");
+    }
+    //static ABC abc = new ABC(); //Eager instantiation
+    static ABC abc = null;
+
+    public static ABC getInstance() {
+
+        //Double check
+        if (abc == null) {
+            synchronized (ABC.class){
+                if(abc == null) {
+                    abc = new ABC();
+                }
+            }
+        }
+        return abc;
+    }
+
+}
---
+++ src/main/java/com/manjunath/dotikol/designpattern/singletone/SingletoneClient.java
@@ -0,0 +1,22 @@
+package com.manjunath.dotikol.designpattern.singletone;
+
+public class SingletoneClient {
+
+    public static void main(String args[]){
+
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                ABC abc = ABC.getInstance();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                ABC abc = ABC.getInstance();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
